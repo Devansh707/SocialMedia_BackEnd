f34f91de06ae3acbdf15d2d29721cb2b71a244c9
Deepak Oraon <deepak.oraon@alumnux.com>
2024-07-26 18:14:58 +0530

    Test

--- Utils/Hashing.js
+++
@@ -1,6 +0,0 @@
-import { hashSync, compareSync } from "bcryptjs";
-
-const createHash = (data) => hashSync(data, 10);
-const compareHash = (data,hash) => compareSync(data, hash);
-
-export default { createHash, compareHash };
--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-import express, { json, urlencoded } from "express";
-import { connect } from "mongoose";
+import express from "express";
 import { config } from "dotenv";
-import userRouter from "./Routes/user.js";
-import postRouter from "./Routes/posts.js";
-import morgan from "morgan";
 import cors from "cors";
-import bodyParser from "body-parser";
 //Setting up express
 const app = express();
 app.options("*", cors());
 app.use(cors());
-app.use(urlencoded({ limit: "50mb", extended: true }));
-app.use(morgan("dev"));
-app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 config();
 
-app.use("/user", userRouter);
-app.use("/post", postRouter);
+app.post("/", async (req, res) => {
+  const statusCode = getRandomStatusCode();
 
-try {
-  connect(process.env.DB_URL);
-  console.log("Connected to DB");
-  app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`);
-  });
-} catch (error) {
-  console.log(`Error : ${error}`);
+  switch (statusCode) {
+    case 200:
+      res.status(200).send({
+        firstName: "firstName",
+        middleName: "middleName",
+        lastName: "lastName",
+      });
+      break;
+    case 404:
+      res.status(404).send("Not Found");
+      break;
+    case 500:
+      res.status(500).send("Internal Server Error");
+      break;
+    default:
+      res.status(500).send("Unexpected Error");
+  }
+});
+
+function getRandomStatusCode() {
+  const statusCodes = [200, 404, 500];
+  const randomIndex = Math.floor(Math.random() * statusCodes.length);
+  return statusCodes[randomIndex];
 }
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
